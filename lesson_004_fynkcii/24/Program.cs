8fd3e3c66bca4c8d4ee2b91bb2480cae270e05f7
Irina_Baslyk <ira.baslyk@mail.ru>
2022-10-28 11:10:18 +0300

    24

---
+++ lesson_004_fynkcii/24/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+7->28
+4->10
+8->36 */
+
+void GetSumNums(int number){
+    int sum=0;
+    for (int i = 1; i <= number; i++)
+    {
+        sum+=i;
+    }
+    Console.WriteLine(sum);
+}
+Console.WriteLine("Введите число:");
+int a =int.Parse(Console.ReadLine());
+
+GetSumNums(a);
