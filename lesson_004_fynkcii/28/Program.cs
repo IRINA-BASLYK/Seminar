595b25566cec6f6e631976e2c96e7b102bb72ee8
Irina_Baslyk <ira.baslyk@mail.ru>
2022-10-28 11:10:44 +0300

    28

---
+++ lesson_004_fynkcii/28/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Напишите программу, которая принимает на вход число (N) и выдаёт произведение чисел от 1 до N.
+4->24
+5->120*/
+
+void GetSumNums(int number){
+    int sum=1;
+    for (int i = 1; i <= number; i++)
+    {
+        sum*=i;
+    }
+    Console.WriteLine(sum);
+}
+Console.WriteLine("Введите число:");
+int a =int.Parse(Console.ReadLine());
+
+GetSumNums(a);
